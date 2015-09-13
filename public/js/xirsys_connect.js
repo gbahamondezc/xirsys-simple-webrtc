50120589223379fff9d4e502c0dcee1c25203a6f
gbahamondez <gonzalo.bahamondez.c@gmail.com>
2015-09-13 00:04:04 -0300

    init repo

---
+++ public/js/xirsys_connect.js
@@ -0,0 +1,10 @@
+var xirsysConnect = {
+	secure : false,
+	data : {
+		domain:	'zbox-developer-challenge.cl',
+		application: 'zbox-developer-challenge',
+		room : 'default',
+		ident	: 'gbahamondez',
+		secret: '22e2a876-58ec-11e5-b61b-4e9028572e7f',
+	}
+};

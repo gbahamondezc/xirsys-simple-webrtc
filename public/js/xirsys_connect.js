eb8351a59b9b912ac818120a9d8011c63dd0ccb3
gbahamondez <gonzalo.bahamondez.c@gmail.com>
2015-09-13 22:58:58 -0300

    back to proevious credentials

--- public/js/xirsys_connect.js
+++ public/js/xirsys_connect.js
@@ -1,22 +1,22 @@
-// var xirsysConnect = {
-// 	secure : false,
-// 	data : {
-// 		domain:	'zbox-developer-challenge.cl',
-// 		application: 'zbox-developer-challenge',
-// 		room : 'default',
-// 		ident	: 'gbahamondez',
-// 		secret: '22e2a876-58ec-11e5-b61b-4e9028572e7f',
-// 	}
-// };
-
-
 var xirsysConnect = {
 	secure : false,
 	data : {
-		domain:	'zbox-developer-challenge.com',
-		application: 'default',
+		domain:	'zbox-developer-challenge.cl',
+		application: 'zbox-developer-challenge',
 		room : 'default',
-		ident	: 'gbahamondezc',
-		secret: '43167f0e-5a82-11e5-8c03-d27d9ff34016',
+		ident	: 'gbahamondez',
+		secret: '22e2a876-58ec-11e5-b61b-4e9028572e7f',
 	}
 };
+
+
+// var xirsysConnect = {
+// 	secure : false,
+// 	data : {
+// 		domain:	'zbox-developer-challenge.com',
+// 		application: 'default',
+// 		room : 'default',
+// 		ident	: 'gbahamondezc',
+// 		secret: '43167f0e-5a82-11e5-8c03-d27d9ff34016',
+// 	}
+// };

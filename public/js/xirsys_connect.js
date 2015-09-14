aac3aa1e4d30df65a339a9358847cd6c19032268
gbahamondez <gonzalo.bahamondez.c@gmail.com>
2015-09-14 15:47:42 -0300

    changes

--- public/js/xirsys_connect.js
+++ public/js/xirsys_connect.js
@@ -1,22 +1,25 @@
-var xirsysConnect = {
-	secure : false,
-	data : {
-		domain:	'zbox-developer-challenge.cl',
-		application: 'zbox-developer-challenge',
-		room : 'default',
-		ident	: 'gbahamondez',
-		secret: '22e2a876-58ec-11e5-b61b-4e9028572e7f',
-	}
-};
-
-
 // var xirsysConnect = {
 // 	secure : false,
 // 	data : {
-// 		domain:	'zbox-developer-challenge.com',
-// 		application: 'default',
+// 		domain:	'zbox-developer-challenge.cl',
+// 		application: 'zbox-developer-challenge',
 // 		room : 'default',
-// 		ident	: 'gbahamondezc',
-// 		secret: '43167f0e-5a82-11e5-8c03-d27d9ff34016',
+// 		ident	: 'gbahamondez',
+// 		secret: '61424ac2-5b0d-11e5-a201-87d76363d0b6',
 // 	}
 // };
+
+var xirsysConnect = {
+	secure : true,
+	token_url : 'https://service.xirsys.com/signal/token',
+	ice_url   : 'https://service.xirsys.com/ice',
+	room_url  : 'https://service.xirsys.com/room',
+
+	data : {
+		domain :	'zbox-developer-challenge.cl',
+		application : 'zbox-developer-challenge',
+		room : 'default',
+    ident : 'gbahamondez',
+    secret : '57f5a5ce-5b10-11e5-ad59-eee59bc40d2b'
+	}
+};
